d4c1e22212c32364f8080f317982824789c45b18
Smashkat12 <smashkat12@gmail.com>
2020-12-25 04:24:44 +0200

    Checkout page with basket subtotal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import Checkout from './Checkout';
 
 function App() {
   return (
-    <div className="app">
-	  <Header />
-	  <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Switch>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
