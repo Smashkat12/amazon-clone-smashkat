cfa9554ccc33e45703076d35a92f0e7aa03ebb8b
Smashkat12 <smashkat12@gmail.com>
2020-12-25 06:03:01 +0200

    added basket total funtion

--- src/Product.js
+++ src/Product.js
@@ -1,31 +1,27 @@
-import React from 'react'
-import "./Product.css"
-import { useStateValue } from './StateProvider';
+import React from "react";
+import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
-function Product({ id, title, image, price , rating}) {
-	
-	const [{basket}, dispatch] = useStateValue();
+function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
 
-	console.log("this is the basket >>>>", basket);
-	
-	const addToBasket = () => {
-		//dispatch the item into the data layer
-		dispatch({
-			type: 'ADD_TO_BASKET',
-			item: {
-				id: id,
-				title: title,
-				image: image,
-				price: price,
-				rating: rating,
-			},
-		});
+  console.log("this is the basket >>>>", basket);
 
+  const addToBasket = () => {
+    //dispatch the item into the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
 
-	};
-
-
-	return (
+  return (
     <div className="product">
       <div className="product__info">
         <p>{title}</p>
@@ -47,4 +43,4 @@ function Product({ id, title, image, price , rating}) {
   );
 }
 
-export default Product
+export default Product;
